46cdfd5c4c79be13b33000eee36dc69ef691f31f
Kali Kiger <kalikiger@gmail.com>
2014-10-27 15:52:28 -0700

    My thoughts on where to go next. My computer is taking forever to set up Xcode.

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,7 @@ module.exports = {
       host     : process.env.APP_DB_HOST     || '127.0.0.1',
       user     : process.env.APP_DB_USER     || '',
       password : process.env.APP_DB_PASSWORD || '',
-      database : process.env.APP_DB_NAME     || 'practice'
+      database : process.env.APP_DB_NAME     || 'cityHacker'
     }
   },
 
