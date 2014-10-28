fe99353a5d92880e66a6e97bd9222ed69794ef58
Stephen Young <StephenJayYoung@gmail.com>
2014-10-28 16:21:23 -0700

    added code to api, still a work in progress

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -146,5 +146,59 @@ App.SignupController = Ember.ObjectController.extend({
 // export default NavigationController;
 
 
-// expose App globally
+//Code below is for integrating the gmap API
+
+CityHacker.gmapAPIRoute = Ember.Route.extend({
+  
+//   // Code from previous project
+//   // model: function()
+//   // var data =
+//   //     near: 'Portland, OR', // TODO: how to change this to work from something on the page?
+//   //     section: 'food',
+//   //     venuePhotos: '1',
+
+//       // required stuffs
+//     //   v: '20141002',
+//     //   client_id: 'THF0PIAQPEPL3UJZJGVVXKL5S1FM4P54MGZARXUFJ1ZGBENP',
+//     //   client_secret: 'PAY5FKCOQB4NI0CSL5XDXNL1AOA2CA2CWTACTYILINBMK4S0'
+//     // }; --}}
+    var url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDk7gb7P3R4gyAbx_p2MeiTwc90BAxDIWM;
+    return Ember.$.ajax"({
+      type: 'GET',
+      url: url,
+      data: data,
+      dataType: 'JSON'
+    })
+//     // .then(function(data) {
+//      // var url = data.response.groups[0].items[0].venue.photos.groups[0].items[0].prefix;
+//      //  console.log(data);
+//      //  console.log(data.response);
+//      //  console.log(data.response.groups[0]);
+//      //  console.log(data.response.groups[0].items);
+//       // console.log(data.response.groups[0].items[0]);
+//       // console.log(data.response.groups[0].items[0].venue);
+//       // console.log(data.response.groups[0].items[0].venue.photos);
+//       // console.log(data.response.groups[0].items[0].venue.photos.groups[0]);
+//       // console.log(data.response.groups[0].items[0].venue.photos.groups[0].items[0].prefix;
+
+//       // var photoInfo = data.response.groups[0].items[0].venue.photos.groups[0].items[0];
+//       // var prefix = photoInfo.prefix;
+//       // var suffix = photoInfo.suffix;
+//       // var size = "300x300";
+//       // var url = prefix + size + suffix;
+
+
+//       //console log the path, get a url, make sure that url works, set that path equal to url, it will show up in browser
+//       // TODO: this should be something from data
+//       // var url = 'https://www.google.com/images/srpr/logo11w.png';
+//       // return {photoURL: url};
+//     });
+  })
+// // });
+
+
+
+
+
+// // expose App globally
 window.App = App;
