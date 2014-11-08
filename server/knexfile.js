0b92222065e58843e417050cd0396332e522643f
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-11-07 16:22:38 -0800

    Updated knexfile.

--- server/knexfile.js
+++ server/knexfile.js
@@ -28,34 +28,12 @@ module.exports = {
 
   staging: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL
   }
 
 };
