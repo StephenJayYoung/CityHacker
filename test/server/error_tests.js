a3b15dc9dda348ac8280396777bc9e33dd65df39
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-12-05 14:59:14 -0800

    changed error code

--- test/server/error_tests.js
+++ test/server/error_tests.js
@@ -58,7 +58,7 @@ describe('API for errors', __app(app, function(H) {
     .done(done, done);
     // adds or changes visible name, interests, location, email, and pictures
   });
-  it.skip('does not allow changes to a user that is not the one signed in.',
+  it('does not allow changes to a user that is not the one signed in.',
    function(done) {
     H.setupDatabase(User, 'users/user-no-token', 'database-users')
     .then(function() { return H.testAPI('users/user-no-token'); })
