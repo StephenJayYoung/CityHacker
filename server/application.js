be4cbb5b30f0823f7f66c33b0935015240073ee4
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-12-15 17:29:15 -0800

    trying to add see self

--- server/application.js
+++ server/application.js
@@ -54,7 +54,7 @@ var prepareUser = function(user) {
   return user;
 };
 
-var prepareUserFriendship = function(user) {
+var prepareUserWithEmail = function(user) {
   var email = user.user_email;
   user = _.omit(user, 'passwordDigest');
   user = _.extend(user, {
@@ -312,7 +312,7 @@ api.get('/users/:id', admit.extract, function(req, res) {
   var sendResponse = function(user) {
     var response = user.toJSON();
     if (usersAreFriends) {
-      response = prepareUserFriendship(response);
+      response = prepareUserWithEmail(response);
     }
     else {
       response = prepareUser(response);
